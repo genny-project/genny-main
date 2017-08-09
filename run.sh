6210b36fe905ff07877dfd2d2624f2b89c176f68
Adam Crow <adamcrow63@gmail.com>
2017-08-09 11:44:27 +1000

    Updated keycloak url

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 ENV_FILE=genny.env
 ./create_genny_env.sh ${ENV_FILE}
-echo "HAZELCAST_XML=-Dvertx.hazelcast.config=./cluster-dockerv2.xml" >> ${ENV_FILE}
+echo "HAZELCAST_XML=-Dvertx.hazelcast.config=./cluster.xml" >> ${ENV_FILE}
 ENV_FILE=$ENV_FILE docker-compose up -d
 ENV_FILE=$ENV_FILE docker-compose logs -f bridge
 #ENV_FILE=$ENV_FILE docker-compose logs -f bridge rulesservice social kie-client
