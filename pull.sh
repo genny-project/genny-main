4468bd700b5eeda20ba7eb5c0a7e013adacd47cb
adamcrow64 <adamcrow63@gmail.com>
2017-08-14 18:02:47 +1000

    Removed layout

--- pull.sh
+++ pull.sh
@@ -5,7 +5,6 @@ docker pull gennyproject/keycloak:working
 docker pull gennyproject/bridge:latest
 docker pull gennyproject/rulesservice:latest
 docker pull gennyproject/social:latest
-docker pull gennyproject/layout:latest
 docker pull gennyproject/alyson:latest
 docker pull gennyproject/scoring:latest
 
