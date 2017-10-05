98f43b41c04a846c3a06be5d66b0a58225a8232b
andres <helios@andress-MBP.lan>
2017-10-05 21:19:58 +1100

    Add Env for the google api

--- create_genny_env.sh
+++ create_genny_env.sh
@@ -65,7 +65,9 @@ KIE_SERVER_URL="http://${myip}:8230/kie-server/services/rest/server"
 KIE_USERNAME="kieserver"
 KIE_PASSWORD="kieserver1!"
 
+GOOGLE_CLIENT_SECRET="{\"installed\":{\"client_id\":\"260075856207-9d7a02ekmujr2bh7i53dro28n132iqhe.apps.googleusercontent.com\",\"project_id\":\"genny-sheets-181905\",\"auth_uri\":\"https:\/\/accounts.google.com\/o\/oauth2\/auth\",\"token_uri\":\"https:\/\/accounts.google.com\/o\/oauth2\/token\",\"auth_provider_x509_cert_url\":\"https:\/\/www.googleapis.com\/oauth2\/v1\/certs\",\"client_secret\":\"vgXEFRgQvh3_t_e5Hj-eb6IX\",\"redirect_uris\":[\"urn:ietf:wg:oauth:2.0:oob\",\"http:\/\/localhost\"]}}"
 
+GOOGLE_SHEETID="1VSXJUn8_BHG1aW0DQrFDnvLjx_jxcNiD33QzqO5D-jc"
 
 DEVUSER=${USER}
 RULES_DIR=/opt/rules
@@ -76,6 +78,8 @@ JAVA_OPTS="-Xms256m -Xmx512m -Djava.net.preferIPv4Stack=true"
 echo "HOSTIP=${myip}" > $ENV_FILE
 echo "DEVUSER=${DEVUSER}" >> $ENV_FILE
 echo "" >> $ENV_FILE
+echo "GOOGLE_SHEETID=${GOOGLE_SHEETID}" >> $ENV_FILE
+echo "GOOGLE_CLIENT_SECRET=${GOOGLE_CLIENT_SECRET}" >> $ENV_FILE
 echo "REACT_APP_PROJECT_NAME=${REACT_APP_PROJECT_NAME}" >> $ENV_FILE
 echo "REACT_APP_QWANDA_API_URL=${REACT_APP_QWANDA_API_URL}" >> $ENV_FILE
 echo "REACT_APP_VERSION_NUMBER=${REACT_APP_VERSION_NUMBER}" >> $ENV_FILE
