e1bc58c13c47713365226c0c7a05f23e25ebadc1
adamcrow64 <adamcrow63@gmail.com>
2017-10-22 23:00:31 +1100

    Removed wildfly-swarm-keycloak-example realm settings

--- run-dev.sh
+++ run-dev.sh
@@ -2,5 +2,7 @@
 
 ENV_FILE=genny.env
 ./create_genny_env.sh ${ENV_FILE} $1
-echo "DEBUG=true" >> ${ENV_FILE}
+echo "DEBUG=TRUE" >> ${ENV_FILE}
+echo "DEBUG_SUSPEND=y" >> ${ENV_FILE}
+echo "GENNYDEV=TRUE" >> ${ENV_FILE}
 ENV_FILE=$ENV_FILE docker-compose -f docker-compose-dev.yml up -d
