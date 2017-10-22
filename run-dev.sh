674546181a009c10bcd38f0f2a89e7ab5f465a6d
adamcrow64 <adamcrow63@gmail.com>
2017-10-22 13:21:39 +1100

    Updated dev scripts

--- run-dev.sh
+++ run-dev.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 ENV_FILE=genny.env
-
 ./create_genny_env.sh ${ENV_FILE} $1
-#echo "HAZELCAST_XML=-Dvertx.hazelcast.config=./cluster-dev.xml" >> ${ENV_FILE}
-
+echo "DEBUG=true" >> ${ENV_FILE}
 ENV_FILE=$ENV_FILE docker-compose -f docker-compose-dev.yml up -d
