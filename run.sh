c937eece69c828f7ca3bc79c02414e3a9caf6196
Sudan <sudansth@gmail.com>
2017-10-24 12:49:46 +1100

    Adding section to read dev.env file as second argument and adding it to the ENV file

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 ENV_FILE=genny.env
+LOCAL_ENV_FILE=$2
+
 ./create_genny_env.sh ${ENV_FILE} $1
+
+#Add any dev env file if exists
+if [ $LOCAL_ENV_FILE ]; then
+   cat ${LOCAL_ENV_FILE} >> $ENV_FILE
+fi
+
 echo "HAZELCAST_XML=-Dvertx.hazelcast.config=./cluster.xml" >> ${ENV_FILE}
 ENV_FILE=$ENV_FILE docker-compose up -d
 ENV_FILE=$ENV_FILE docker-compose logs -f bridge rulesservice
