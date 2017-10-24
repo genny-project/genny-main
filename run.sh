afc7134764880129ffdae285d32426931950d9bb
Sudan <sudansth@gmail.com>
2017-10-24 16:05:25 +1100

    Updating run.sh to read realm name as string in 2nd commandline argument instead of a file

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ENV_FILE=genny.env
-LOCAL_ENV_FILE=$2
+LOCAL_REALM_NAME="PROJECT_REALM=$2"
 
 ./create_genny_env.sh ${ENV_FILE} $1
 
 #Add any dev env file if exists
-if [ $LOCAL_ENV_FILE ]; then
-   cat ${LOCAL_ENV_FILE} >> $ENV_FILE
+if [ $LOCAL_REALM_NAME ]; then
+    echo  $LOCAL_REALM_NAME  >> $ENV_FILE
 fi
 
 echo "HAZELCAST_XML=-Dvertx.hazelcast.config=./cluster.xml" >> ${ENV_FILE}
