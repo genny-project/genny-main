19abd5d8c3298da90a2da7c6f401f05280bfb03e
Sudan <sudansth@gmail.com>
2017-10-26 14:17:22 +1100

    Added docker logs of qwanda-service

--- run.sh
+++ run.sh
@@ -13,5 +13,5 @@ fi
 echo "HAZELCAST_XML=-Dvertx.hazelcast.config=./cluster.xml" >> ${ENV_FILE}
 
 ENV_FILE=$ENV_FILE docker-compose up -d
-ENV_FILE=$ENV_FILE docker-compose logs -f bridge rulesservice
+ENV_FILE=$ENV_FILE docker-compose logs -f bridge rulesservice qwanda-service
 #ENV_FILE=$ENV_FILE docker-compose logs -f bridge rulesservice social kie-client
