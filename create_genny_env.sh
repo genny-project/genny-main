9812f5de3b6585de93918568346ed0c2f7324917
andres <helios@andress-MBP.lan>
2017-11-15 18:55:04 +1100

    added GOOGLE_HOSTING_SHEET_ID to create_env

--- create_genny_env.sh
+++ create_genny_env.sh
@@ -65,7 +65,7 @@ KIE_PASSWORD="kieserver1!"
 
 GOOGLE_CLIENT_SECRET="{\"installed\":{\"client_id\":\"260075856207-9d7a02ekmujr2bh7i53dro28n132iqhe.apps.googleusercontent.com\",\"project_id\":\"genny-sheets-181905\",\"auth_uri\":\"https:\/\/accounts.google.com\/o\/oauth2\/auth\",\"token_uri\":\"https:\/\/accounts.google.com\/o\/oauth2\/token\",\"auth_provider_x509_cert_url\":\"https:\/\/www.googleapis.com\/oauth2\/v1\/certs\",\"client_secret\":\"vgXEFRgQvh3_t_e5Hj-eb6IX\",\"redirect_uris\":[\"urn:ietf:wg:oauth:2.0:oob\",\"http:\/\/localhost\"]}}"
 
-GOOGLE_SHEETID="1VSXJUn8_BHG1aW0DQrFDnvLjx_jxcNiD33QzqO5D-jc"
+GOOGLE_HOSTING_SHEET_ID="1HAppJufvePWSiSyvPkxNfZp6NHdB8PANeH1IJopdEsE"
 
 DEVUSER=${USER}
 RULES_DIR=/opt/rules
