8dae3223c81e1adb9e6642aa8164567b3ff9ab69
Sudan <sudansth@gmail.com>
2017-11-28 13:41:30 +1100

    added run dev functionlity for secrets

--- run-dev.sh
+++ run-dev.sh
@@ -4,6 +4,27 @@ ENV_FILE=genny.env
 LOCAL_REALM_NAME="PROJECT_REALM=$2"
 
 ./create_genny_env.sh ${ENV_FILE} $1
+CREDENTIALS_GENNY="  ../credentials/credentials-genny"
+
+CREDENTIALS="credentials"
+
+if [ ! -d "$CREDENTIALS" ]; then
+   mkdir ../$CREDENTIALS
+fi
+
+#Add any dev env file if exists
+if [ $LOCAL_REALM_NAME ]; then
+    echo  $LOCAL_REALM_NAME  >> $ENV_FILE
+fi
+
+./fetch_github_env.sh ${ENV_FILE}
+
+if [ ! -d "$CREDENTIALS_GENNY" ]; then
+        cd ../$CREDENTIALS
+        git clone https://github.com/OutcomeLife/credentials-genny.git
+        cd -
+   cat "../credentials/credentials-genny/genny_conf.env" >> $ENV_FILE
+fi
 
 echo "DEBUG=TRUE" >> ${ENV_FILE}
 echo "DEBUG_SUSPEND=y" >> ${ENV_FILE}
