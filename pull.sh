93e2f300239eb2b329a517ed9c9f84a79fa78bc7
Adam Crow <acrow@Adams-Compooter.local>
2017-12-20 18:05:30 +1100

    Updated docker pulls

--- pull.sh
+++ pull.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 docker pull gennyproject/wildfly-qwanda-service:latest
-docker pull gennyproject/keycloak:working
+docker pull gennyproject/keycloak:latest
 docker pull gennyproject/bridge:latest
 docker pull gennyproject/rulesservice:latest
 docker pull gennyproject/social:latest
 docker pull gennyproject/alyson:latest
 docker pull gennyproject/scoring:latest
+docker pull gennyproject/messages:latest
+
 
