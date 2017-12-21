fccddd0f1cdef4786f0428e0e5a8d72799c0485d
rpgayatri <rpgayatri@gmail.com>
2017-12-21 11:04:14 +1100

    removing messageSheetId, twilio-target, email-target from env variables

--- create_genny_env.sh
+++ create_genny_env.sh
@@ -64,8 +64,6 @@ KIE_SERVER_URL="http://${myip}:8230/kie-server/services/rest/server"
 KIE_USERNAME="kieserver"
 KIE_PASSWORD="kieserver1!"
 
-MESSAGES_SHEETID=1zm0DbjYhZJm-An3C10vY_c4N6jeSgEhXNLrUtxBx5lE
-
 #GOOGLE_CLIENT_SECRET="{\"installed\":{\"client_id\":\"260075856207-9d7a02ekmujr2bh7i53dro28n132iqhe.apps.googleusercontent.com\",\"project_id\":\"genny-sheets-181905\",\"auth_uri\":\"https:\/\/accounts.google.com\/o\/oauth2\/auth\",\"token_uri\":\"https:\/\/accounts.google.com\/o\/oauth2\/token\",\"auth_provider_x509_cert_url\":\"https:\/\/www.googleapis.com\/oauth2\/v1\/certs\",\"client_secret\":\"vgXEFRgQvh3_t_e5Hj-eb6IX\",\"redirect_uris\":[\"urn:ietf:wg:oauth:2.0:oob\",\"http:\/\/localhost\"]}}"
 
 #GOOGLE_HOSTING_SHEET_ID="1HAppJufvePWSiSyvPkxNfZp6NHdB8PANeH1IJopdEsE"
@@ -75,17 +73,15 @@ RULES_DIR=/opt/rules
 
 # Twilio test account credentials
 
-TWILIO_ACCOUNT_SID=AC6bd0bd2ecad9cc99d01a14942e0095a4
-TWILIO_AUTH_TOKEN=e984da10977d62c51c9e5d0c5662a377
+TWILIO_ACCOUNT_SID=
+TWILIO_AUTH_TOKEN=
 
-TWILIO_SOURCE_PHONE=+61488807705
-TWILIO_TARGET_PHONE=+61416805402
+TWILIO_SOURCE_PHONE=
 
 # Email credentials
 
 EMAIL_USERNAME=
 EMAIL_PASSWORD=
-EMAIL_TARGET=
 
 # Mail Transfer Protocol variables
 
@@ -124,10 +120,6 @@ echo "MAIL_SMTP_AUTH=${MAIL_SMTP_AUTH}" >> $ENV_FILE
 echo "MAIL_SMTP_STARTTLS_ENABLE=${MAIL_SMTP_STARTTLS_ENABLE}" >> $ENV_FILE
 echo "MAIL_SMTP_HOST=${MAIL_SMTP_HOST}" >> $ENV_FILE
 echo "MAIL_SMTP_PORT=${MAIL_SMTP_PORT}" >> $ENV_FILE
-echo "EMAIL_TARGET=${EMAIL_TARGET}" >> $ENV_FILE
-echo "TWILIO_TARGET_PHONE=${TWILIO_TARGET_PHONE}" >> $ENV_FILE
-echo "MESSAGES_SHEETID=${MESSAGES_SHEETID}" >> $ENV_FILE
-
 
 echo "" >> $ENV_FILE
 echo "KEYCLOAKPORT=${KEYCLOAK_PORT}" >> $ENV_FILE
