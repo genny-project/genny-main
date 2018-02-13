350ae9f9b3ea82f7d86b001d0ca646230d331888
devjs <loris1634@gmail.com>
2018-02-14 10:22:35 +1100

    mentor match launcher

---
+++ run-mentor-dev.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+ENV_FILE=genny.env
+LOCAL_REALM_NAME="PROJECT_REALM=$2"
+
+./create_genny_env.sh ${ENV_FILE} $1
+
+CREDENTIALS="$HOME/.genny/credentials"
+CREDENTIALS_GENNY="$CREDENTIALS/credentials-mentormatch"
+if [ ! -d "$CREDENTIALS" ]; then
+   mkdir -p $CREDENTIALS
+fi
+
+#Add any dev env file if exists
+#if [ $LOCAL_REALM_NAME ]; then
+#    echo  $LOCAL_REALM_NAME  >> $ENV_FILE
+#fi
+
+#./fetch_github_env.sh ${ENV_FILE}
+
+cat "$CREDENTIALS_GENNY/conf.env" >> $ENV_FILE
+cat "$CREDENTIALS_GENNY/StoredCredential" > google_credentials/StoredCredential
+
+echo "DEBUG=TRUE" >> ${ENV_FILE}
+echo "DEBUG_SUSPEND=y" >> ${ENV_FILE}
+echo "GENNYDEV=TRUE" >> ${ENV_FILE}
+
+#Add any dev env file if exists
+if [ $LOCAL_REALM_NAME ]; then
+    echo  $LOCAL_REALM_NAME  >> $ENV_FILE
+fi
+
+
+ENV_FILE=$ENV_FILE docker-compose -f docker-compose-dev.yml up -d
+ENV_FILE=$ENV_FILE docker-compose -f docker-compose-dev.yml logs -f qwanda-service 
