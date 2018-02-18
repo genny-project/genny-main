39f24fe4e37dd590ddb2a07687072ad28ec59a8f
Adam Crow <acrow@Adams-Compooter.local>
2018-02-19 09:56:49 +1100

    removed payment settings from create_genny_env

---
+++ run-dev2.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+ENV_FILE=genny.env
+LOCAL_REALM_NAME="PROJECT_REALM=$2"
+
+./create_genny_env.sh ${ENV_FILE} $1
+
+CREDENTIALS="$HOME/.genny/credentials"
+CREDENTIALS_GENNY="$CREDENTIALS/credentials-genny"
+if [ ! -d "$CREDENTIALS" ]; then
+   mkdir -p $CREDENTIALS
+fi
+
+#Add any dev env file if exists
+#if [ $LOCAL_REALM_NAME ]; then
+#    echo  $LOCAL_REALM_NAME  >> $ENV_FILE
+#fi
+
+#./fetch_github_env.sh ${ENV_FILE}
+
+if [ ! -d "$CREDENTIALS_GENNY" ]; then
+        cd $CREDENTIALS
+        git clone https://github.com/genny-project/credentials-genny.git
+        cd -
+        cat "$CREDENTIALS_GENNY/genny_conf.env" >> $ENV_FILE
+        cat "$CREDENTIALS_GENNY/StoredCredential" > google_credentials/StoredCredential
+else
+   cat "$CREDENTIALS_GENNY/genny_conf.env" >> $ENV_FILE
+   cat "$CREDENTIALS_GENNY/StoredCredential" > google_credentials/StoredCredential
+fi
+
+echo "DEBUG=TRUE" >> ${ENV_FILE}
+echo "DEBUG_SUSPEND=y" >> ${ENV_FILE}
+echo "GENNYDEV=TRUE" >> ${ENV_FILE}
+
+#Add any dev env file if exists
+if [ $LOCAL_REALM_NAME ]; then
+    echo  $LOCAL_REALM_NAME  >> $ENV_FILE
+fi
+
+
+ENV_FILE=$ENV_FILE docker-compose -f docker-compose-dev2.yml up -d
+ENV_FILE=$ENV_FILE docker-compose -f docker-compose-dev2.yml logs -f qwanda-service 
