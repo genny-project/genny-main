a359ef684110e75e393d8013220d3d2930020022
Adam Crow <acrow@Adams-Compooter.local>
2018-05-10 19:50:05 +1000

    Fixed mode in run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 customercode=${1}
 mode={2}
-./run-setup.sh -p ${customercode} -r ${customercode} -n 10.123.123.123 dev
+./run-setup.sh -p ${customercode} -r ${customercode} -n 10.123.123.123 ${mode} 
