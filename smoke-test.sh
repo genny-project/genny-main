7a20b108eec52493baa2a14fc84884c6e33c25fc
Adam Crow <acrow@Adams-Compooter.local>
2018-07-24 14:42:41 +1000

    Cleaned up

--- smoke-test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ../../
-PWD=`pwd`
-
-docker run -e WEB_URL=http://v2.channel40.com.au/ -v $PWD/tests/smokeTest:/home/robot-framework/tests/smokeTest -v $PWD/reports:/home/robot-framework-logs -v $PWD/resources:/home/robot-framework/resources -v $PWD/screenshots:/home/robot-framework/screenshots rohitcs/rohit-robot
