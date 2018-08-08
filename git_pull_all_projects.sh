507d82958b2c4a43960e8b5ee165bdf78fefe7d5
Adam Crow <acrow@Adams-Compooter.local>
2018-08-08 11:40:21 +1000

    Updated docker-compose

--- git_pull_all_projects.sh
+++ git_pull_all_projects.sh
@@ -4,8 +4,14 @@ function git_project {
  project=$1
  pushd .
  cd ..
- echo "Synching in project ${project}"
- if cd ${project}; then git pull; else git clone https://github.com/genny-project/${project} ${project}; fi
+echo "Synching in project ${project}"
+if cd ${project}; then 
+    git stash;
+    git checkout master
+    git pull; 
+else 
+    git clone https://github.com/genny-project/${project} ${project}; 
+fi
  popd
 }
 
@@ -23,13 +29,18 @@ git_project keycloak
 git_project keycloak-themes
 git_project kie-client
 git_project qwanda
-git_project qwanda-services
-git_project wildfly-qwanda-service
 git_project qwanda-utils
 git_project genny-verticle 
+git_project qwanda-services
+git_project wildfly-qwanda-service
 git_project alyson-v2
 git_project qwanda-ql
 git_project uppy
 git_project in-app-calling
+git_project prj_channel40
+git_project prj_fourdegrees
+git_project prj_internmatch
+git_project prj_butler
+
 
 echo "Finished loading in all the projects"
