3298eef09bbef302b895c9abdf8ff92a0f4a3e2a
c7yrus <c7yrus@gmail.com>
2018-11-27 16:34:40 +1100

    added the tests folder and generic folders inside of it

---
+++ tests/index.js
@@ -0,0 +1,18 @@
+import { Login } from './Login';
+import { Logout } from './Logout';
+import { Registration } from './Registration';
+
+/*Login() 
+.then(() => {
+
+return Logout();
+})
+.then(() => {
+  return Registration();
+})
+.catch(e => {
+  console.error (e );
+})
+
+export default this;
+export.modules.id = "Authentification";*/
