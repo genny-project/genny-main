8cf9347b9fd4ddd9473644b192604cf95d400bce
Adam Crow <adamcrow63@gmail.com>
2019-06-18 10:00:37 +1000

    Added cyrusFetch.sh

---
+++ cyrusFetch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+git pull
+docker pull gennyproject/dummy:latest
+docker pull gennyproject/rulesservice:dummy
+docker pull gennyproject/wildfly-rulesservice:dummy
+docker pull gennyproject/wildfly:v2.4.0
+
+
+cd ../qwanda
+git pull;mvn install -DskipTests=true
+cd ../qwanda-utils
+git pull;mvn install -DskipTests=true
+cd ../genny-verticle-rules
+git pull;mvn install -DskipTests=true
+cd ../genny-rules
+git pull;mvn install -DskipTests=true
+cd ../qwanda-services
+git pull;mvn install -DskipTests=true
+cd ../wildfly-qwanda-service
+git pull;mvn package -DskipTests=true
+./build-docker.sh
+git pull;cd ../wildfly-rulesservice
+mvn package -DskipTests=true
+./build-docker.sh
+cd ../bridge
+git pull;mvn package -DskipTests=true
+./build-docker.sh
+
