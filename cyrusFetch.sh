0b6054e4fc23ff71a2acf7641ca45ff45213222a
Adam Crow <adamcrow63@gmail.com>
2019-06-30 18:05:51 +1000

    Fixed wildfly-rulesservice build

--- cyrusFetch.sh
+++ cyrusFetch.sh
@@ -8,30 +8,41 @@ docker pull gennyproject/wildfly:v2.4.0
 
 cd ../qwanda
 git pull;mvn install -DskipTests=true
+
 cd ../qwanda-utils
 git pull;mvn install -DskipTests=true
+
 cd ../genny-verticle-rules
 git pull;mvn install -DskipTests=true
+
 cd ../genny-rules
 git pull;mvn install -DskipTests=true
+
 cd ../qwanda-services
 git pull;mvn install -DskipTests=true
+
 cd ../wildfly-qwanda-service
 git pull;mvn package -DskipTests=true
 ./build-docker.sh
-git pull;cd ../wildfly-rulesservice
-mvn package -DskipTests=true
+
+cd ../wildfly-rulesservice
+git pull;mvn package -DskipTests=true
 ./build-docker.sh
+
 cd ../bridge
 git pull;mvn package -DskipTests=true
 ./build-docker.sh
+
 cd ../messages
 git pull;mvn package -DskipTests=true
 ./build-docker.sh
+
 cd ../rulesservice
 git pull;mvn package -DskipTests=true
 ./build-docker.sh
+
 cd ../prj_genny
 git pull
+
 cd ../genny-main
 
