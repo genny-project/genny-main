86204f646bc45c0acda6938446968770ef9d76d9
Adam Crow <adamcrow63@gmail.com>
2019-06-30 19:27:53 +1000

    Added cyrusStatus.sh

---
+++ cyrusStatus.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ver=v2.4.0
+
+cd ../qwanda;echo $PWD;git branch
+cd ../qwanda-utils;echo $PWD;git branch
+cd ../genny-verticle-rules;echo $PWD;git branch
+cd ../qwanda-services;echo $PWD;git branch
+cd ../genny-rules;echo $PWD;git branch
+cd ../wildfly-rulesservice;echo $PWD;git branch
+cd ../rulesservice;echo $PWD;git branch
+cd ../wildfly-qwanda-service;echo $PWD;git branch
+cd ../checkrules;echo $PWD;git branch
+cd ../bridge;echo $PWD;git branch
+cd ../messages;echo $PWD;git branch
+cd ../prj_genny;echo $PWD;git branch
+
+cd genny-main
+
