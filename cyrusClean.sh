75713fdb23a8c98f5f95b65585c284de37f1070f
Adam Crow <adamcrow63@gmail.com>
2019-09-04 12:15:39 +1000

    Added a fuiller build cyrus

---
+++ cyrusClean.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd ../qwanda
+mvn clean install -DskipTests=true
+cd ../qwanda-utils
+mvn clean install -DskipTests=true
+cd ../genny-verticle-rules
+mvn clean install -DskipTests=true
+cd ../genny-rules
+mvn clean install -DskipTests=true
+cd ../qwanda-services
+mvn clean install -DskipTests=true
+cd ../wildfly-qwanda-service
+mvn clean package -DskipTests=true
+./build-docker.sh
+cd ../wildfly-rulesservice
+mvn clean package -DskipTests=true
+./build-docker.sh
+cd ../bridge
+mvn clean package -DskipTests=true
+./build-docker.sh
+
