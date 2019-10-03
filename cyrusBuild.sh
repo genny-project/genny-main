7537d10bc1e7712ccbc0afd915fd59478881ad1e
Adam Crow <adamcrow63@gmail.com>
2019-10-03 10:17:51 +1000

    Updated cyrusBuild to be faster

--- cyrusBuild.sh
+++ cyrusBuild.sh
@@ -20,10 +20,7 @@ mvn package -DskipTests=true
 cd ../bridge
 mvn package -DskipTests=true
 ./build-docker.sh
-cd ../media-proxy
-mvn clean package -DskipTests=true
-./build-docker.sh
-cd ../media-proxy
-mvn package -DskipTests=true
-./build-docker.sh
+#cd ../media-proxy
+#mvn package -DskipTests=true
+#./build-docker.sh
 cd ../genny-main
