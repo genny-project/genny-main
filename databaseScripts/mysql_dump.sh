911f50bd00a940046a27355f1d774f60dfacb2de
Adam Crow <adamcrow63@gmail.com>
2019-11-11 14:26:01 +1100

    Updated mysql_dump.sh top handle utf8mb4

--- databaseScripts/mysql_dump.sh
+++ databaseScripts/mysql_dump.sh
@@ -2,6 +2,6 @@
 today=`date '+%Y_%m_%d__%H_%M_%S'`;
 filename="genny-$today.bz2"
 echo $filename;
-mysqldump  --single-transaction --order-by-primary --tz-utc --opt  -P 3310 -h 127.0.0.1 -u genny --password=password --databases gennydb  -r  ../backup.sql 
+mysqldump  --default-character-set=utf8mb4   --single-transaction --order-by-primary --tz-utc --opt  -P 3310 -h 127.0.0.1 -u genny --password=password --databases gennydb  -r  ../backup.sql 
 tar cvfj  ../$filename  ../backup.sql
 
