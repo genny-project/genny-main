44edbc4c91dd72206f6a87ec16729238e629c91a
Adam Crow <adamcrow63@gmail.com>
2019-11-22 15:15:35 +1100

    Updated restart.sh to synch cache as well. Added cyrusSyncCache

--- restart.sh
+++ restart.sh
@@ -13,4 +13,5 @@ ENV_FILE=genny.env
 ENV_FILE=$ENV_FILE docker-compose stop $@ 
 ENV_FILE=$ENV_FILE docker-compose rm -f $@ 
 ENV_FILE=$ENV_FILE docker-compose up -d $@ 
+./cyrusSyncCache.sh
 ENV_FILE=$ENV_FILE docker-compose logs -f $@ 
