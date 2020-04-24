bff400f9b41b8cd1d2d64bac5f1836574ea251ab
Jian Li <hbjoylee@gmail.com>
2020-04-24 16:02:34 +1000

    Fix bootxport build order

--- cyrusBuild.sh
+++ cyrusBuild.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 cd ../qwanda
 mvn install -DskipTests=true
+
 cd ../qwanda-utils
 mvn install -DskipTests=true
-cd ../genny-verticle-rules
-mvn install -DskipTests=true
-cd ../genny-rules
-mvn install -DskipTests=true
+
 cd ../bootxport
 mvn install -DskipTests=true
+
+cd ../genny-verticle-rules
+mvn install -DskipTests=true
+
+cd ../genny-rules
+mvn install -DskipTests=true
+
 cd ../qwanda-services
 mvn install -DskipTests=true
+
 cd ../wildfly-qwanda-service
 mvn package -DskipTests=true
 ./build-docker.sh
+
 cd ../wildfly-rulesservice
 mvn package -DskipTests=true
 ./build-docker.sh
+
 cd ../bridge
 mvn package -DskipTests=true
 ./build-docker.sh
+
 #cd ../media-proxy
 #mvn package -DskipTests=true
 #./build-docker.sh
