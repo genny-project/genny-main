5089c35efef7ebd80cf0b34bff647fbf44a96746
Jian Li <hbjoylee@gmail.com>
2020-06-26 01:19:00 +0000

    clean up

--- cyrusStatus.sh
+++ cyrusStatus.sh
@@ -17,19 +17,4 @@ do
     fi
 done
 
-#cd ../qwanda;echo $PWD;echo BRANCH:`git rev-parse --abbrev-ref HEAD`;echo STATUS:`git status`
-#cd ../qwanda-utils;echo $PWD;git rev-parse --abbrev-ref HEAD;git status
-#cd ../genny-verticle-rules;echo $PWD;git rev-parse --abbrev-ref HEAD;git status
-#cd ../bootxport;echo $PWD;git rev-parse --abbrev-ref HEAD;git status
-#cd ../qwanda-services;echo $PWD;git rev-parse --abbrev-ref HEAD;git status
-#cd ../genny-rules;echo $PWD;git rev-parse --abbrev-ref HEAD;git status
-#cd ../wildfly-rulesservice;echo $PWD;git rev-parse --abbrev-ref HEAD;git status
-##cd ../rulesservice;echo $PWD;git rev-parse --abbrev-ref HEAD;git status
-#cd ../wildfly-qwanda-service;echo $PWD;git rev-parse --abbrev-ref HEAD;git status
-#cd ../checkrules;echo $PWD;git rev-parse --abbrev-ref HEAD;git status
-#cd ../bridge;echo $PWD;git rev-parse --abbrev-ref HEAD;git status
-#cd ../media-proxy;echo $PWD;git rev-parse --abbrev-ref HEAD;git status
-#cd ../messages;echo $PWD;git rev-parse --abbrev-ref HEAD;git status
-#cd ../prj_genny;echo $PWD;git rev-parse --abbrev-ref HEAD;git status
 cd ../genny-main
-
