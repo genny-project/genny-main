6cc7487ab50af5a0b0a67ce03fa7397d2a954d1b
Adam Crow <adamcrow63@gmail.com>
2020-07-22 13:24:44 +1000

    Fixed db

---
+++ cyrusStatus2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ver=7.0.0
+
+cd ../qwanda;echo $PWD;git branch;git status
+cd ../qwanda-utils;echo $PWD;git branch;git status
+cd ../genny-verticle-rules;echo $PWD;git branch;git status
+cd ../bootxport;echo $PWD;git branch;git status
+cd ../qwanda-services;echo $PWD;git branch;git status
+cd ../genny-rules;echo $PWD;git branch;git status
+cd ../wildfly-rulesservice;echo $PWD;git branch;git status
+#cd ../rulesservice;echo $PWD;git branch;git status
+cd ../wildfly-qwanda-service;echo $PWD;git branch;git status
+cd ../checkrules;echo $PWD;git branch;git status
+cd ../bridge;echo $PWD;git branch;git status
+cd ../media-proxy;echo $PWD;git branch;git status
+cd ../messages;echo $PWD;git branch;git status
+cd ../prj_genny;echo $PWD;git branch;git status
+cd ../genny-main
