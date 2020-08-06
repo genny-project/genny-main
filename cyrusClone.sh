0439ce6f536bdc2410ac04b3f9cdb05396af0b08
Adam Crow <adamcrow63@gmail.com>
2020-08-06 19:26:53 +1000

    Updated keycloak-hu to keycloak for media-proxy

--- cyrusClone.sh
+++ cyrusClone.sh
@@ -18,14 +18,14 @@ git clone -b ${ver} https://github.com/genny-project/qwanda-services
 git clone -b ${ver} https://github.com/genny-project/bootxport 
 git clone -b ${ver} https://github.com/genny-project/wildfly-rulesservice 
 git clone -b ${ver} https://github.com/genny-project/wildfly-qwanda-service 
-git clone -b ${ver} https://github.com/genny-project/prj_genny 
-git clone -b ${ver} https://github.com/genny-project/checkrules 
-git clone -b ${ver} https://github.com/genny-project/rulesservice
-git clone -b ${ver} https://github.com/genny-project/bridge
-git clone -b ${ver} https://github.com/genny-project/media-proxy
-git clone -b ${ver} https://github.com/genny-project/messages
-git clone -b ${ver} https://github.com/genny-project/alyson-v7
-git clone -b ${ver} https://github.com/genny-project/gennyteer
+#git clone -b ${ver} https://github.com/genny-project/prj_genny 
+#git clone -b ${ver} https://github.com/genny-project/checkrules 
+#git clone -b ${ver} https://github.com/genny-project/rulesservice
+#git clone -b ${ver} https://github.com/genny-project/bridge
+#git clone -b ${ver} https://github.com/genny-project/media-proxy
+#git clone -b ${ver} https://github.com/genny-project/messages
+#git clone -b ${ver} https://github.com/genny-project/alyson-v7
+#git clone -b ${ver} https://github.com/genny-project/gennyteer
 
 cd genny-main
 
