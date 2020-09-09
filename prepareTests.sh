3d8ccdd7bf620c20b88e2ce0994875d6e57b1f29
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2020-09-09 14:46:52 +1000

    preparing test environment script

--- prepareTests.sh
+++ prepareTests.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 pathDir=$1
+boilerPlate=~/projects/genny/prj_internmatch/tests/interaction/helpers/boilerplate.js
+
+if [ "x""${pathDir}" == "x" ]
+then
+    echo "### PLEASE PASS A TEST NAME ###";
+    exit 1;
+fi
 
 echo "### Preparing Testing Environment ###";
 
@@ -20,12 +27,10 @@ if [ -d ~/projects/genny/prj_internmatch/ ]
 then
     cd ~/projects/genny/prj_internmatch;
     git pull;
-
 else
     cd ~/projects/genny;
     git clone https://github.com/OutcomeLife/prj_internmatch.git;
     cd ~/projects/genny/prj_internmatch;
-
 fi
 
 cd ~/projects/genny/prj_internmatch/tests/interaction/tests;
@@ -40,4 +45,7 @@ then
     touch ${final_pathDir}
 else
     echo "### Path Dir does not exist ###";
+    exit 1;
 fi
+
+cat ${boilerPlate} >> ${final_pathDir}
