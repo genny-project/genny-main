1e773750c306d72f2dac0af92c0e222d5a3081f2
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2020-09-09 11:44:12 +1000

    preparing test environment script

--- prepareTests.sh
+++ prepareTests.sh
@@ -7,10 +7,21 @@ then
     cd ~/projects/genny/gennyteer;
     git pull;
     ./build-docker.sh;
-    echo "### Gennyteer exists ###";
 else
     cd ~/projects/genny;
     git clone https://github.com/genny-project/gennyteer.git;
     cd ~/projects/genny/gennyteer;
     ./build-docker.sh;
+fi
+
+if [ -d ~/projects/genny/prj_internmatch/ ]
+then
+    cd ~/projects/genny/prj_internmatch;
+    git pull;
+    
+else
+    cd ~/projects/genny;
+    git clone https://github.com/OutcomeLife/prj_internmatch.git;
+    cd ~/projects/genny/prj_internmatch;
+
 fi
