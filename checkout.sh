385067e26e1d8a945f37b3f6671fe3e045852a2b
Jian Li <hbjoylee@gmail.com>
2020-09-24 04:58:48 +0000

    checkout to v7.1.0

--- checkout.sh
+++ checkout.sh
@@ -1,6 +1,7 @@
+VERSION=v7.1.0
 for i in ` find .. -mindepth 1 -maxdepth 1 -type d | awk -F "/" '{ print $2 }'`;do
     cd ../$i
-    git checkout v7.0.0;git pull
+    git pull ; git checkout $VERSION
     cd ../genny-main
     echo $i
 done
