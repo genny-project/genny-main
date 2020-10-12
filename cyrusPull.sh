f4538094e7c26b71ac7584d9355d93d56b7d8b04
Adam Crow <adamcrow63@gmail.com>
2020-10-12 15:57:36 +1100

    Fixed cd

--- cyrusPull.sh
+++ cyrusPull.sh
@@ -3,10 +3,10 @@
 for value in qwanda qwanda-utils bootxport genny-verticle-rules genny-rules qwanda-services wildfly-rulesservice wildfly-qwanda-service checkrules bridge media-proxy messages alyson gennyteer 
 do
     echo $value
-    cd ../$value 
+    cd ../${value} 
     git stash
     git pull
-    cd genny-main
+    cd ../genny-main
 done
 
 for i in ` find .. -mindepth 1 -maxdepth 1 -type d | grep prj  | awk -F "/" '{ print $2 }'`;do
