3a072a9da3f47363b59139cec043c78a0c485409
Adam Crow <adamcrow63@gmail.com>
2020-10-13 17:02:42 +1100

    Fixed cyrusBuild

--- cyrusBuild.sh
+++ cyrusBuild.sh
@@ -6,7 +6,7 @@ do
     sudo rm -Rf  ../$value/target/*
     cd ../$value
     mvn $clean install -DskipTests=true
-    cd genny-main
+    cd ../genny-main
 done
 
 for value in wildfly-qwanda-service/qwanda-service-war wildfly-qwanda-service/qwanda-service-ear wildfly-ruleservice/rulesservice-war wildfly-ruleservice/rulesservice-ear 
@@ -14,7 +14,7 @@ do
     echo $value
     cd ../$value
     sudo rm -Rf  ../$value/target/*
-    cd genny-main
+    cd ../genny-main
 done
 
 for value in wildfly-qwanda-service wildfly-ruleservice bridge checkrules media-proxy messages 
@@ -24,6 +24,6 @@ do
     cd ../$value
     mvn $clean package -DskipTests=true
     ./build-docker.sh
-    cd genny-main
+    cd ../genny-main
 done
 
