b11db4ff9f7966a8371016a01a23db16852087a8
Jian Li <hbjoylee@gmail.com>
2020-10-21 09:06:39 +1100

    update checkout.sh to accept argument

--- checkout.sh
+++ checkout.sh
@@ -1,4 +1,9 @@
-VERSION=v7.1.0
+VERSION=$1
+if [[ -z $VERSION ]];then
+    echo "you need pass branch name as argument, e.g checkout.sh v7.1.0"
+    exit 1
+fi
+
 for i in ` find .. -mindepth 1 -maxdepth 1 -type d | awk -F "/" '{ print $2 }'`;do
     cd ../$i
     git pull ; git checkout $VERSION
