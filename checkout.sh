bdec87b3e9d37bb9addb00383a552761c9b20ac5
Jian Li <hbjoylee@gmail.com>
2020-10-21 10:58:44 +1100

    fix wildfly-rulesservice

--- checkout.sh
+++ checkout.sh
@@ -4,10 +4,10 @@ if [ -z $VERSION ];then
     exit 1
 fi
 
-for i in alyson qwanda qwanda-utils bootxport genny-verticle-rules genny-rules qwanda-services wildfly-qwanda-service wildfly-ruleservice bridge checkrules media-proxy messages notes abn-lite  
+for i in alyson qwanda qwanda-utils bootxport genny-verticle-rules genny-rules qwanda-services wildfly-qwanda-service wildfly-rulesservice bridge checkrules media-proxy messages notes abn-lite  
 do
     echo $i
     cd ../$i
-    git pull ; git checkout $VERSION
+    git pull ; git checkout $VERSION ; git pull
     cd ../genny-main
 done
