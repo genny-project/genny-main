cba58694ea198e003ba341ee042be1c695abdc4f
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2020-10-29 13:32:37 +1100

    update genny project version

--- updateGennyProjectVersion.sh
+++ updateGennyProjectVersion.sh
@@ -10,6 +10,7 @@ OLD_VERSION=${1}
 NEW_VERSION=${2}
 GITHUB_TOKEN=${3}
 AUTH=genny-project
+PRIVATE_AUTH=OutcomeLife
 
 # Concatenating
 NEW_BRANCH="${NEW_VERSION}"
@@ -35,9 +36,11 @@ fi
 
 repos=("genny-main" "qwanda" "qwanda-utils" "genny-verticle-rules" "bootxport" "qwanda-services"
 "genny-rules" "wildfly" "wildfly-rulesservice" "wildfly-qwanda-service" "checkrules"  "bridge" "notes" "shleemy"
-"media-proxy" "messages" "prj_genny" "prj_stt" "prj_internmatch" )
+"media-proxy" "messages" "prj_genny" )
 
 repos2=("alyson", "gennyteer")
+
+repos3=("prj_stt" "prj_internmatch")
 
 # check jq
 which jq
@@ -48,6 +51,8 @@ else
     echo "Can not find jq, please install first."
     exit 1
 fi
+
+echo "### repos set 1 ###"
 
 for REPO in "${repos[@]}"; do
     # Create new branch
@@ -62,6 +67,8 @@ for REPO in "${repos[@]}"; do
 
 done
 
+echo "### repos set 2 ###"
+
 for REPO in "${repos2[@]}"; do
     # Create new branch
     SHA=$(curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/${AUTH}/${REPO}/git/refs/heads/${PREVIOUS_BRANCH} | jq -r '.object.sha')
@@ -74,6 +81,22 @@ for REPO in "${repos2[@]}"; do
     curl  -X PUT -H "Authorization: token $GITHUB_TOKEN"  -H "Accept: application/vnd.github.luke-cage-preview+json"  https://api.github.com/repos/${AUTH}/${REPO}/branches/${NEW_BRANCH}/protection -d '{"required_status_checks":{"strict":true, "contexts":["continuous-integration/jenkins"]},"enforce_admins":false,"required_pull_request_reviews":{"required_approving_review_count":1}, "restrictions": null}'
 
 done
+
+echo "### repos set 3 ###"
+
+for REPO in "${repos3[@]}"; do
+    # Create new branch
+    SHA=$(curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/${PRIVATE_AUTH}/${REPO}/git/refs/heads/${PREVIOUS_BRANCH} | jq -r '.object.sha')
+    curl -X POST -H "Authorization: token $GITHUB_TOKEN"  -d  "{\"ref\": \"refs/heads/${NEW_BRANCH}\",\"sha\": \"$SHA\"}"  https://api.github.com/repos/${PRIVATE_AUTH}/${REPO}/git/refs
+
+    # set default branch
+    curl -X PATCH -H "Authorization: token $GITHUB_TOKEN"  -d  "{\"name\": \"${REPO}\",\"default_branch\": \"$NEW_BRANCH\"}"  https://api.github.com/repos/${PRIVATE_AUTH}/${REPO}
+
+    # set branch protection rule
+    curl  -X PUT -H "Authorization: token $GITHUB_TOKEN"  -H "Accept: application/vnd.github.luke-cage-preview+json"  https://api.github.com/repos/${PRIVATE_AUTH}/${REPO}/branches/${NEW_BRANCH}/protection -d '{"required_status_checks":{"strict":true, "contexts":["continuous-integration/jenkins"]},"enforce_admins":false,"required_pull_request_reviews":{"required_approving_review_count":1}, "restrictions": null}'
+
+done
+
 echo "### Maven Update ###"
 
 if [ -z "$NEW_VERSION" ]
