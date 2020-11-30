c0c7dcf450b6039c6359510cc11da3d66c653fb6
Jian Li <hbjoylee@gmail.com>
2020-11-30 04:51:57 +0000

    adjust path in cyrusPull.sh

--- cyrusPull.sh
+++ cyrusPull.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
+parentdir="$(dirname `pwd`)"
+
 for value in qwanda qwanda-utils bootxport genny-verticle-rules genny-rules qwanda-services wildfly-rulesservice wildfly-qwanda-service checkrules bridge media-proxy messages notes shleemy  alyson gennyteer 
 do
     echo $value
-    cd ../${value} 
+    cd $parentdir/${value} 
     git stash
     git pull
-    cd ../genny-main
 done
 
 for i in ` find .. -mindepth 1 -maxdepth 1 -type d | grep prj  | awk -F "/" '{ print $2 }'`;do
-   cd ../$i
-   git stash;git pull
-   cd ../genny-main 
    echo $i
+   cd $parentdir/$i
+   git stash;git pull
 done
-
-
-
