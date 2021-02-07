a30176f707141eea1c1c94daeb05118719d2fdef
Adam Crow <adamcrow63@gmail.com>
2021-02-08 10:06:23 +1100

    Updated cyrusStatus2

--- cyrusStatus2.sh
+++ cyrusStatus2.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-ver=7.8.0
+ver=7.18.0
 
-cd ../qwanda;echo $PWD;git branch;git status
-cd ../qwanda-utils;echo $PWD;git branch;git status
-cd ../genny-verticle-rules;echo $PWD;git branch;git status
-cd ../bootxport;echo $PWD;git branch;git status
-cd ../qwanda-services;echo $PWD;git branch;git status
-cd ../genny-rules;echo $PWD;git branch;git status
-cd ../wildfly-rulesservice;echo $PWD;git branch;git status
-#cd ../rulesservice;echo $PWD;git branch;git status
-cd ../wildfly-qwanda-service;echo $PWD;git branch;git status
-cd ../checkrules;echo $PWD;git branch;git status
-cd ../bridge;echo $PWD;git branch;git status
-cd ../media-proxy;echo $PWD;git branch;git status
-cd ../messages;echo $PWD;git branch;git status
-cd ../prj_genny;echo $PWD;git branch;git status
+cd ../qwanda;echo $PWD;git branch;git status | grep '*'
+cd ../qwanda-utils;echo $PWD;git branch;git status | grep '*'
+cd ../genny-verticle-rules;echo $PWD;git branch;git status| grep '*'
+cd ../bootxport;echo $PWD;git branch;git status| grep '*'
+cd ../qwanda-services;echo $PWD;git branch;git status| grep '*'
+cd ../genny-rules;echo $PWD;git branch;git status| grep '*'
+cd ../wildfly-rulesservice;echo $PWD;git branch;git status| grep '*'
+#cd ../rulesservice;echo $PWD;git branch;git status| grep '*'
+cd ../wildfly-qwanda-service;echo $PWD;git branch;git status| grep '*'
+cd ../checkrules;echo $PWD;git branch;git status| grep '*'
+cd ../bridge;echo $PWD;git branch;git status| grep '*'
+cd ../media-proxy;echo $PWD;git branch;git status| grep '*'
+cd ../messages;echo $PWD;git branch;git status| grep '*'
+cd ../prj_genny;echo $PWD;git branch;git status| grep '*'
 cd ../genny-main
