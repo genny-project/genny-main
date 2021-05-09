ad4c834818ef14e94635c9a41437a43133a00b9a
Adam Crow <adamcrow63@gmail.com>
2021-05-10 09:09:26 +1000

    Added keystore gen

---
+++ setup-keystore.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+keytool -genkeypair -storepass password -storetype PKCS12 -keyalg RSA -keysize 2048 -dname "CN=server" -alias server -ext "SAN:c=DNS:localhost,IP:127.0.0.1" -keystore conf/server.keystore
+
