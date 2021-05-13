2bf3b24a7400f48cec6002f855899c91f4931a31
Adam Crow <adamcrow63@gmail.com>
2021-05-13 10:06:38 +1000

    Updated to build jprofiler

--- restart-quick.sh
+++ restart-quick.sh
@@ -9,9 +9,17 @@ for i in ` find .. -mindepth 1 -maxdepth 1 -type d | grep prj  | awk -F "/" '{ p
   echo $i
 done
 
+# Set the cache for SKIP
+TOKEN=`../cmd/scripts/gettoken-prod.sh`
+#echo $TOKEN
+#echo ""
+echo "******** WRITE CACHE **********"
+DB=`curl -s  -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' --header "Authorization: Bearer $TOKEN"  --header "Cache-Control: no-cache" -d "TRUE"  "http://alyson7.genny.life:8280/service/cache/write/internmatch/SKIP"`
+DB=`curl -s  -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' --header "Authorization: Bearer $TOKEN"  --header "Cache-Control: no-cache" -d "TRUE"  "http://alyson7.genny.life:8280/service/cache/write/internmatch/SKIPBOOTXPORT"`
+
 ENV_FILE=genny.env
 ENV_FILE=$ENV_FILE docker-compose stop wildfly-rulesservice qwanda-service 
 ENV_FILE=$ENV_FILE docker-compose rm -f  wildfly-rulesservice qwanda-service
 ENV_FILE=$ENV_FILE docker-compose up -d wildfly-rulesservice qwanda-service 
-./cyrusSyncCache.sh
+#./cyrusSyncCache.sh
 ENV_FILE=$ENV_FILE docker-compose logs -f $@ 
