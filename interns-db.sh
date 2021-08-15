4ccffb8627aba171bdb7c449dbcb2bdcd9256b94
Jian Li <hbjoylee@gmail.com>
2021-08-16 09:56:07 +1000

    use utc timezone for s3 script

--- interns-db.sh
+++ interns-db.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export TZ="UTC"
 DATE=$(date +%Y_%m_%d)
 latestBackup=$(aws s3 ls s3://internmatch-backups/interns-backup/$DATE/  | tail -1 | awk '{print $4}')
 aws s3 cp s3://internmatch-backups/interns-backup/$DATE/$latestBackup .
@@ -7,7 +8,7 @@ aws s3 cp s3://internmatch-backups/interns-backup/$DATE/$latestBackup .
 tar xvf $latestBackup
 
 db=$(ls tmp)
-sed -i '/@@GLOBAL.GTID_PURGED=/d'    tmp/$db
-sed -i '/@MYSQLDUMP_TEMP_LOG_BIN/d'   tmp/$db
+#sed -i '/@@GLOBAL.GTID_PURGED=/d'    tmp/$db
+#sed -i '/@MYSQLDUMP_TEMP_LOG_BIN/d'   tmp/$db
 
 rm $latestBackup
--- restore-db.sh
+++ restore-db.sh
@@ -6,7 +6,9 @@ if [[ -z $ip ]];then
         exit 1
 fi
 
+export TZ="UTC"
 DATE=$(date +%Y_%m_%d)
+echo $DATE
 latestBackup=$(aws s3 ls s3://internmatch-backups/interns-backup/$DATE/  | tail -1 | awk '{print $4}')
 aws s3 cp s3://internmatch-backups/interns-backup/$DATE/$latestBackup .
 
