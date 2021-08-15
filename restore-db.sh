1b1eb5d23e7b987956d3a8fa7f1c03a9633494fe
Jian Li <hbjoylee@gmail.com>
2021-08-15 23:27:09 +0000

    add restore-db.sh

---
+++ restore-db.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ip=$1
+if [[ -z $ip ]];then
+        echo "Please pass your ip as argument, e.g ./restore-latest-db.sh 192.168.1.2"
+        exit 1
+fi
+
+DATE=$(date +%Y_%m_%d)
+latestBackup=$(aws s3 ls s3://internmatch-backups/interns-backup/$DATE/  | tail -1 | awk '{print $4}')
+aws s3 cp s3://internmatch-backups/interns-backup/$DATE/$latestBackup .
+
+rm -rf tmp
+
+tar xvf $latestBackup
+
+db=$(ls tmp)
+#sed -i '/@@GLOBAL.GTID_PURGED=/d'    tmp/$db
+#sed -i '/@MYSQLDUMP_TEMP_LOG_BIN/d'   tmp/$db
+
+mysql -h $ip -u genny --password=password  gennydb  -e "drop database gennydb;"
+mysql -h $ip -u genny --password=password < tmp/$db
+
+rm $latestBackup
