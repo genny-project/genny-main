3311b88ef25c8714c689ecc2c7a1dc6eba8b05fb
Jasper Robison <jasper.robison22@gmail.com>
2022-02-18 16:28:11 +1100

    clean up and better formatting

--- cyrusStatus.sh
+++ cyrusStatus.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-folders=("qwanda" "qwandaq" "qwanda-utils" "genny-verticle-rules" "bootxport" "qwanda-services" 
-        "genny-rules" "wildfly-rulesservice" "wildfly-qwanda-service" "checkrules" "bridge" "fyodor" "dropkick" "lauchy" "messages" "notes" "shleemy" "bootq" "genny-proxy"
-        "media-proxy" "prj_genny" "prj_internmatch")
+# define a newline char
 NEWLINE=$'\n'
 
-# Reset
+# define our colours
 Color_Off='\033[0m'       # Text Reset
-
-# Regular Colors
 Black='\033[0;30m'        # Black
 Red='\033[0;31m'          # Red
 Green='\033[0;32m'        # Green
@@ -18,13 +14,25 @@ Purple='\033[0;35m'       # Purple
 Cyan='\033[0;36m'         # Cyan
 White='\033[0;37m'        # White
 
+
+# define our list of directories
+folders=("qwanda" "qwandaq" "qwanda-utils" "genny-verticle-rules" "bootxport" "qwanda-services" 
+        "genny-rules" "wildfly-rulesservice" "wildfly-qwanda-service" "checkrules" "bridge" "fyodor" "dropkick" "lauchy" "messages" "notes" "shleemy" "bootq" "genny-proxy"
+        "media-proxy" "prj_genny" "prj_internmatch")
+
+
+# begin iterating directories
 for dirname in ${folders[@]};
 do 
     if [[ -d ../$dirname ]]; then
         cd ../${dirname}; 
+
+		# output the directory and the branch it is on in nice colours
 		printf Project:\ ${Blue}$dirname${Color_Off}\ \ \ \ Branch:\ ${Green}`git rev-parse --abbrev-ref HEAD`${Color_Off}
         echo ${NEWLINE}
-		git status
+
+		# output the status, cutting the first 4 lines out
+		git -c color.status=always status | tail +4
         echo ${NEWLINE}
     else
         echo "$dirname does not exist!!!" 
