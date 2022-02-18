de6dd6b03b251ff606ba9c326e098b7507e9ebd4
Jasper Robison <jasper.robison22@gmail.com>
2022-02-18 16:14:16 +1100

    clearer view of project, branch and status

--- cyrusStatus.sh
+++ cyrusStatus.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
-folders=("qwanda" "qwanda-utils" "genny-verticle-rules" "bootxport" "qwanda-services" 
-        "genny-rules" "wildfly-rulesservice" "wildfly-qwanda-service" "checkrules" "lauchy"  "bridge"  "notes" "shleemy" "bootq" "genny-proxy"
-        "media-proxy" "messages" "prj_genny" "prj_internmatch")
+folders=("qwanda" "qwandaq" "qwanda-utils" "genny-verticle-rules" "bootxport" "qwanda-services" 
+        "genny-rules" "wildfly-rulesservice" "wildfly-qwanda-service" "checkrules" "bridge" "fyodor" "dropkick" "lauchy" "messages" "notes" "shleemy" "bootq" "genny-proxy"
+        "media-proxy" "prj_genny" "prj_internmatch")
 NEWLINE=$'\n'
+
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
 
 for dirname in ${folders[@]};
 do 
     if [[ -d ../$dirname ]]; then
-        #echo "Project:$dirname"
         cd ../${dirname}; 
-        echo Project:$dirname,BRANCH:`git rev-parse --abbrev-ref HEAD`, STATUS:`git status`
-        #echo CURRENTDIR:$PWD, BRANCH:`git rev-parse --abbrev-ref HEAD`, STATUS:`git status`
-        #echo ${NEWLINE}
+		printf Project:\ ${Blue}$dirname${Color_Off}\ \ \ \ BRANCH:\ ${Green}`git rev-parse --abbrev-ref HEAD`${Color_Off}
+        echo ${NEWLINE}
+		echo STATUS:
+		git status
+        echo ${NEWLINE}
     else
         echo "$dirname does not exist!!!" 
     fi
