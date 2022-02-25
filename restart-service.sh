2b476ed00ec85c1171d630df11a82b039b8e11ef
Daniel Mills <daniel@millsit.com>
2022-02-25 15:00:44 +1100

    Patched restart-service.sh file

--- restart-service.sh
+++ restart-service.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 ENV_FILE=genny.env docker-compose stop   $@
 ENV_FILE=genny.env docker-compose rm -f $@
-ENF_FILE=genny.env docker-compose up -d $@
+ENV_FILE=genny.env docker-compose up -d $@
