39e79c46b9cc5ed7817d8b642b3fe4b5015b0825
Expressionless <rhysmchaen@gmail.com>
2022-03-07 17:06:20 +1100

    Updated kafkacat

--- kafkacat.sh
+++ kafkacat.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-kcat -b kafka:9092 -C -o beginning -q -t $1
+if hash kafkacat 2>/dev/null; then 
+    kafkacat -b kafka:9092 -C -o beginning -q -t $1
+else
+    echo "kafkacat not installed"
+    echo "Ubuntu: sudo apt-get install kafkacat"
+    echo "Fedora: refer to this: https://rmoff.net/2020/04/20/how-to-install-kafkacat-on-fedora/"
+fi
