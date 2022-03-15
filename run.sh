c90fcfe976f48984721da3e77395aabca221da01
Adam Crow <adamcrow63@gmail.com>
2022-03-15 11:57:21 +1100

    Added protobuf and svg copying

--- run.sh
+++ run.sh
@@ -10,9 +10,61 @@ for i in ` find .. -mindepth 1 -maxdepth 1 -type d | grep prj  | awk -F "/" '{ p
   echo $i
 done
 
+#Copy all the protobufs, and svgs
+PERSISTENCE_FOLDER=./target/protobuf
+KOGITO_TRAVEL_AGENCY_PERSISTENCE=../kogitoq2/kogitoq/extended/travels/target/classes/META-INF/resources/persistence/protobuf
+KOGITO_VISAS_PERSISTENCE=../kogitoq2/kogitoq/extended/visas/target/classes/META-INF/resources/persistence/protobuf
+
+mkdir -p $PERSISTENCE_FOLDER
+
+if [ -d "$KOGITO_TRAVEL_AGENCY_PERSISTENCE" ]
+then
+    cp $KOGITO_TRAVEL_AGENCY_PERSISTENCE/*.proto $PERSISTENCE_FOLDER
+else
+    echo "$KOGITO_TRAVEL_AGENCY_PERSISTENCE does not exist. Have you compiled your Kogito Travel Agency project?"
+    exit 1
+fi
+
+if [ -d "$KOGITO_VISAS_PERSISTENCE" ]
+then
+    cp $KOGITO_VISAS_PERSISTENCE/*.proto $PERSISTENCE_FOLDER
+else
+    echo "$KOGITO_VISAS_PERSISTENCE does not exist. Have you compiled your Kogito Visas project?"
+    exit 1
+fi
+
+SVG_FOLDER=./svg
+
+KOGITO_TRAVEL_SVG_FOLDER=../kogitoq2/kogitoq/extended/travels/target/classes/META-INF/processSVG
+KOGITO_VISAS_SVG_FOLDER=../kogitoq2/kogitoq/extended/visas/target/classes/META-INF/processSVG
+
+mkdir -p $SVG_FOLDER
+
+if [ -d "$KOGITO_TRAVEL_SVG_FOLDER" ]
+then
+    cp $KOGITO_TRAVEL_SVG_FOLDER/*.svg $SVG_FOLDER
+else
+    echo "$KOGITO_TRAVEL_SVG_FOLDER does not exist. Have you compiled Kogito Travel Agency project?"
+    exit 1
+fi
+
+if [ -d "$KOGITO_VISAS_SVG_FOLDER" ]
+then
+    cp $KOGITO_VISAS_SVG_FOLDER/*.svg $SVG_FOLDER
+else
+    echo "$KOGITO_VISAS_SVG_FOLDER does not exist. Have you compiled Kogito Visas project?"
+    exit 1
+fi
+
+
+
 customercode=${1}
 mode=${2}
 #create the ip controlled network
 docker network create --gateway 172.18.0.1 --subnet 172.18.0.0/24 mainproxy
+
+
+
+
 ./run-setup.sh -p ${customercode} -r ${customercode} -n 10.123.123.123 ${mode} 
 ./say.sh "Genny System $1 Finished Loading"
