6de595c872aa2aea7500c749141a57fc293aeb8a
Adam Crow <adamcrow63@gmail.com>
2022-04-05 21:46:29 +1000

    Added forced kogito build

--- cyrusBuild.sh
+++ cyrusBuild.sh
@@ -59,7 +59,8 @@ do
     cd $parentdir/$value
     ./build-docker.sh
 done
-
+cd $parentdir/kogitoq2/kogitoq/extended/travels
+./build.sh;./build-docker.sh
 cd $parentdir/genny-main
 echo "Finished Building all"
 ./say.sh "Completed Building All. Now building docker"
