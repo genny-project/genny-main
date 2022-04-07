9e6cbe2ec4141d568c04e7ea4e1938dcfee70602
Expressionless <rhysmchaen@gmail.com>
2022-04-07 10:26:44 +1000

    Added find-rule.sh

---
+++ cyrusBuildServices.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+parentdir="$(dirname `pwd`)"
+clean=$1
+
+echo "Building qwandaq"
+cd $parentdir/qwandaq
+mvn $clean install -DskipTests=true
+
+echo "Building serviceq"
+cd $parentdir/serviceq
+mvn $clean install -DskipTests=true
+
+for service in fyodor lauchy messages dropkick kogitoq2 adi; do
+        echo $service
+        cd $parentdir/$service
+        mvn $clean install -DskipTests=true
+        ./build-docker.sh
+done
+./say.sh "Completed Building Services"
