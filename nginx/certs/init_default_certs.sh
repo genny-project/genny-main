443583d09c705415341f61491cce24e0753200ec
Adam Crow <adamcrow63@gmail.com>
2022-04-16 11:57:41 +1000

    WARNING: This is set for mentormatch

--- nginx/certs/init_default_certs.sh
+++ nginx/certs/init_default_certs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Generating genny.life self signed certs"
-declare -a arr=("keycloak" "alyson" "alyson3" "alyson2" "bridge" "qwanda-service" "api" "rulesservice" "messages" "social" "uppy" "layout-cache" "twilio" "payments")
+declare -a arr=("keycloak" "alyson" "alyson3" "alyson2" "bridge" "qwanda-service" "api" "rulesservice" "messages" "social" "uppy" "layout-cache" "twilio" "payments" "internmatch" "mentormatch" "credmatch" "lojing")
 for i in "${arr[@]}"
 do
+echo "Generating ${i} cert"
 openssl req -x509 -newkey rsa:2048 -keyout my.pass.key -out ${i}.genny.life.crt -days 1365 -subj "/C=AU/ST=VIC/L=Warracknabeal/O=Genny.Life/OU=Org/CN=${i}.genny.life" -passout pass:ABCDABCD
 openssl rsa -in my.pass.key -out ${i}.genny.life.key -passin pass:ABCDABCD
 rm -f my.pass.key
