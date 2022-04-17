c36226aef38919ee7d62742d986d2c6d03ef0d61
Adam Crow <adamcrow63@gmail.com>
2022-04-17 17:03:28 +1000

    Updated local certs

--- nginx/certs/init_default_certs.sh
+++ nginx/certs/init_default_certs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Generating genny.life self signed certs"
-declare -a arr=("keycloak" "alyson" "alyson3" "alyson2" "bridge" "qwanda-service" "api" "rulesservice" "messages" "social" "uppy" "layout-cache" "twilio" "payments")
+declare -a arr=("keycloak" "alyson" "alyson3" "alyson2" "bridge" "qwanda-service" "api" "rulesservice" "messages" "social" "uppy" "layout-cache" "twilio" "payments" "internmatch" "mentormatch" "lojing" "credmatch")
 for i in "${arr[@]}"
 do
 openssl req -x509 -newkey rsa:2048 -keyout my.pass.key -out ${i}.genny.life.crt -days 1365 -subj "/C=AU/ST=VIC/L=Warracknabeal/O=Genny.Life/OU=Org/CN=${i}.genny.life" -passout pass:ABCDABCD
