0d3bd22c07c8e9f34c488eefb97c066cb304e4fa
Jasper Robison <jasper.robison22@gmail.com>
2022-06-04 12:41:47 +1000

    clean up cyrusBuild and docker-compose

--- cyrusBuild.sh
+++ cyrusBuild.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 set -e
-
 clean=$1
 parentdir="$(dirname `pwd`)"
-#sdk use java g21.0.0.r11-grl
-
 
 # clean and build package
 for value in qwanda qwanda-utils bootxport genny-verticle-rules genny-rules qwanda-services    
 do
     echo $value
-#    rm -Rf $parentdir/$value/target/*
     cd $parentdir/$value
     mvn $clean install -DskipTests=true
 done
 
-# build package build docker image
+# build rulesservice
 for value in wildfly-rulesservice 
-#for value in wildfly-qwanda-service wildfly-rulesservice 
 do
     echo $value
-#    sudo rm -Rf  $parentdir/$value/target/*
     cd $parentdir/$value
     mvn $clean package -DskipTests=true
     ./build-docker.sh
@@ -35,17 +29,8 @@ cd $parentdir/gennyq
 for value in checkrules bootq genny-proxy api2email 
 do
     echo $value
-#    rm -Rf  $parentdir/$value/target/*
     cd $parentdir/$value
     mvn  $clean  package -DskipTests=true
-#    ./build-docker.sh
-done
-
-
-for value in checkrules bootq genny-proxy api2email
-do
-    echo $value
-    cd $parentdir/$value
     ./build-docker.sh
 done
 
