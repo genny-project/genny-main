28cf0d3fbf9d41a8c1bb8024ede0de66f344565d
Adam Crow <adamcrow63@gmail.com>
2022-06-20 10:48:12 +1000

    Updated to get infinispan running

--- kafkacat.sh
+++ kafkacat.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [ "$#" -eq 0 ]; then
 	echo "Usage: $0 <topic>"
-	kafkacat -b localhost -L | grep topic
+	kcat -b localhost -L | grep topic
 	exit 1
 fi
-if hash kafkacat 2>/dev/null; then 
-    kafkacat -b alyson.genny.life:9092 -C -o beginning -q -t $1
+if hash kcat 2>/dev/null; then 
+    kcat -b alyson.genny.life:9092 -C -o beginning -q -t $1
 else
-    echo "kafkacat not installed"
+    echo "kcat not installed"
     echo "Ubuntu: sudo apt-get install kafkacat"
     echo "Fedora: refer to this: https://rmoff.net/2020/04/20/how-to-install-kafkacat-on-fedora/"
 fi
