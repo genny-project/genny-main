3c3279fe77abb9df284f033eeb0be80d05776fcb
varunshastry <varunshastry@rediffmail.com>
2022-06-29 07:35:58 +0530

    Replacing ; with &&

--- cyrusBuild.sh
+++ cyrusBuild.sh
@@ -35,7 +35,7 @@ do
 done
 
 cd $parentdir/gennyq/kogitoq/gadaq
-./build.sh;./build-docker.sh
+./build.sh && ./build-docker.sh
 cd $parentdir/genny-main
 echo "ProductCodes = ${PRODUCT_CODES}"                       
 if [[ -n "$PRODUCT_CODES" ]]
@@ -48,7 +48,7 @@ then
             do  
                 echo "Processing product ${p}"
                 cd ${HOME}/projects/genny/products/prd_${p}
-	        ./build.sh;./build-docker.sh
+	        ./build.sh && ./build-docker.sh
             done
         else
            echo "No Products available"
