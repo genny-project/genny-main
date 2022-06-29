e6ba2658d386d9a1d9a2fa804325960db2884dcc
Adam Crow <adamcrow63@gmail.com>
2022-06-29 14:20:16 +1000

    Brutal name fix for logs

--- logs.sh
+++ logs.sh
@@ -2,6 +2,8 @@
 
 #ENV_FILE=genny.env
 name=$(docker ps -a --format "table {{.Names}}" | grep $1)
+#direct setting to avoid kafka being recognised as kafka-connect 
+name=$1 
 echo "logging ${name}"
 #ENV_FILE=$ENV_FILE docker-compose logs -f  ${name} 
 #ENV_FILE=$ENV_FILE docker-compose logs -f $name 
