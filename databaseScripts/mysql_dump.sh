4c26cbc58d4ca1949c1a5c7f093ff306c95aef65
Adam Crow <adamcrow63@gmail.com>
2022-08-23 09:09:31 +1000

    Enable messages by default and add local mysqldump

--- databaseScripts/mysql_dump.sh
+++ databaseScripts/mysql_dump.sh
@@ -2,7 +2,7 @@
 today=`date '+%Y_%m_%d__%H_%M_%S'`;
 filename="genny-$today.bz2"
 echo $filename;
-mysqldump  --default-character-set=utf8mb4   --single-transaction --order-by-primary --tz-utc --opt  -P 3310 -h 127.0.0.1 -u genny --password=password --databases gennydb  -r  ../../backup.sql 
+mysqldump  --default-character-set=utf8mb4   --single-transaction --order-by-primary --tz-utc --opt  -P 3310 -h 127.0.0.1 -u root --password=password --databases gennydb  -r  ../../backup.sql 
 bzip2 -c ../../backup.sql > ../../$filename 
 #tar cvfj  ../../$filename  ../../backup.sql
 
