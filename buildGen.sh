ceb63f598c0f3e4a922ac8fbe25a9e61a34815b1
vm <vm@gada.io>
2022-11-02 04:49:07 +0000

    Added more validation on DB restore

--- buildGen.sh
+++ buildGen.sh
@@ -20,10 +20,10 @@ color_Off='\033[0m'       # Text Reset
                 ./run.sh dev1 up
 
                 if (( $(echo $?) > 0)); then
-                        echo -e "${red}Fail to run all genny services${color_off}";
-                else
-                        echo -e "${green}Ran successfully${color_off}";
-                        checkCPU
+	        	echo -e "${red}Fail to run all genny services${color_off}";			      
+		else
+			 echo -e "${green}Ran successfully${color_off}";
+                        checkCPU 
                  fi
         }
 
@@ -34,10 +34,11 @@ color_Off='\033[0m'       # Text Reset
                 docker ps
 
                 if (( $(echo $?) > 0)); then
-                         echo -e "${red}Build failed${color_off}";
-                else
-                         echo -e "${green}Build successfully${color_off}";
-                        runGenny
+			     echo -e "${red}Build failed${color_off}";
+			     else
+			 echo -e "${green}Build successfully${color_off}";
+			 echo 'buildgenny';
+                        runGenny 	      
                 fi
         }
 
@@ -51,27 +52,55 @@ color_Off='\033[0m'       # Text Reset
                 #List down the running docker containers
                 docker container ls
                 if [ $( docker ps -a | grep gennyproject/media-proxy:latest | wc -l ) != 0 ]; then
-                        echo -e "${red}Some docker containers exist${color_off}";
-                else
-                        echo -e "${green}Docker container does not exist${color_off}";
-                        buildGenny
+			echo -e "${red}Some docker containers exist${color_off}";
+			else
+			 echo -e "${green}Docker container does not exist${color_off}";
+                        buildGenny 
                 fi
         }
 
+	checkTmpDirectory()
+	{
+		if [[ ! -d /tmp ]];
+			then  echo -e "${red}tmp directory doesn't exist${color_off}";
+				else
+                 restoreDB 
+                fi
+	}
+
+	checkMYSQLContainer()
+	{
+		if [ $( docker ps -a | grep mysql:8.0 | wc -l ) != 0 ]; then
+                        echo -e "${green}MySQL containers exist${color_off}";
+                        else
+                         echo -e "${red}MYSQL container does not exist${color_off}";
+			 cd ../genny-main/
+                        ./run-setup.sh mysql
+                fi
+
+		if (( $(echo $?) > 0)); then
+                        echo -e "${red}Fail to get latest${color_off}";
+                 else
+                        echo -e "${green}Get latest has been successful${color_off}";
+                #       restoreDB
+                	checkTmpDirectory
+                fi
+	} 
+
         restoreDB()
-        {
+        {	
                 cd ../genny-main/
                 echo -e "${blue}--Restoring the DataBase with latest backup from the production${color_off}";
                 #Find the existing sql file from the tmp directory
                 sqlFile=$(find ./tmp/* -name "*.sql");
-                echo -e "${green}--SQL file:------${color_off}" $sqlFile ;
+               # echo -e "${green}--SQL file:------${color_off}" $sqlFile ;
                 #Get the size of the existing sql file
                 fileSize=$(ls -l $sqlFile | awk '{print $5}');
                 echo -e "${green}First sql file size${color_off}" $fileSize;
 
                 #Restore the database
-                ./restore-db.sh internmatch
-
+       		 #./restore-db.sh internmatch
+		./restore-db.sh alyson.genny.life
                 #Find the restored sql file from the tmp directory
                 restoredFile=$(find ./tmp/* -name "*.sql");
                 echo -e "${green}--Restored SQL file:-----${color_off}" $restoredFile;
@@ -81,15 +110,19 @@ color_Off='\033[0m'       # Text Reset
 
                 #Compare the file sizes
                  if [[ $fileSize -ne $resSize ]]; then
-                        echo -e "${green}--Succesfully restored the database${color_off}";
+			echo -e "${green}--Succesfully restored the database${color_off}";
                         stopContainers
-                elif [[ $fileSize = $resSize ]]; then
-                        echo -e "${green}--Already restored the current database backup${color_off}";
-                        stopContainers
-                 else
-                        echo -e "${red}--Fail to restore the DB${color_off}";
+		       
+		elif [[ $fileSize = $resSize ]]; then
+			echo -e "${green}--Already restored the current database backup${color_off}";
+                        stopContainers 
+		else
+			echo -e "${red}--Fail to restore the DB${color_off}";
                         #Show the latest backups
-                        ~/latest_gennydb_backups.sh
+                       # ~/latest_gennydb_backups.sh 
+		 fi
+	      	 if (( $(echo $?) > 0)); then
+                       echo -e "${red}--Fail to restore the DB${color_off}";      
                 fi
         }
 
@@ -104,10 +137,11 @@ color_Off='\033[0m'       # Text Reset
                 done
 
                 if (( $(echo $?) > 0)); then
-                        echo -e "${red}Fail to get latest${color_off}";
-                else
-                        echo -e "${green}Get latest has been successful${color_off}";
-                       restoreDB
+			 echo -e "${red}Fail to get latest${color_off}";	 
+		 else
+			  echo -e "${green}Get latest has been successful${color_off}";
+               
+		checkMYSQLContainer 
                 fi
         }
         getlatest
