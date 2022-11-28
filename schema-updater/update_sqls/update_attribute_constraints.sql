c530e1809b0b0b1bb8483f333e21add976b96e95
varunshastry <varunshastry@rediffmail.com>
2022-11-28 15:18:58 +1100

    Schema changes and BE cache-defn changes

--- schema-updater/update_sqls/update_attribute_constraints.sql
+++ schema-updater/update_sqls/update_attribute_constraints.sql
@@ -1,3 +1,4 @@
 ALTER TABLE attribute ADD CONSTRAINT unique_key UNIQUE KEY(id);
 ALTER TABLE attribute DROP PRIMARY KEY;
 ALTER TABLE attribute ADD CONSTRAINT pri_key PRIMARY KEY (realm,code);
+ALTER TABLE baseentity MODIFY dtype VARCHAR(31);
--- schema-updater/update_sqls/update_baseentity_constraints.sql
+++ schema-updater/update_sqls/update_baseentity_constraints.sql
@@ -1,3 +1,4 @@
 ALTER TABLE baseentity ADD CONSTRAINT unique_key UNIQUE KEY(id);
 ALTER TABLE baseentity DROP PRIMARY KEY;
 ALTER TABLE baseentity ADD CONSTRAINT pri_key PRIMARY KEY (realm,code);
+ALTER TABLE baseentity MODIFY dtype VARCHAR(31);
