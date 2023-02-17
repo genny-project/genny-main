289b6a43203cc68741bb00cf25ba7a347e073c36
varunshastry <varunshastry@rediffmail.com>
2023-02-17 11:47:25 +1100

    Caching for datatype and validation

--- schema-updater/update_sqls/create_datatype.sql
+++ schema-updater/update_sqls/create_datatype.sql
@@ -6,11 +6,17 @@ CREATE TABLE IF NOT EXISTS datatype (
   classname varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   component varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   inputmask varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  validation_codes varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (realm,dttcode)
 ) ENGINE=InnoDB AUTO_INCREMENT=7092 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO datatype (realm, dttcode, classname, component, inputmask)
-SELECT DISTINCT realm, dttcode, classname, component, inputmask FROM `attribute` WHERE dttcode IS NOT NULL AND component IS NOT NULL;
+INSERT INTO datatype (realm, dttcode, classname, component, inputmask, validation_codes)
+SELECT distinct realm, dttcode, classname, component, inputmask, SUBSTR(validation_list, 2, POSITION('"' IN SUBSTR(validation_list, 2, CHAR_LENGTH(validation_list)))-1) AS validation_codes  
+FROM `attribute` WHERE dttcode IS NOT NULL AND component IS NOT NULL AND 
+SUBSTR(validation_list, 2, POSITION('"' IN SUBSTR(validation_list, 2, CHAR_LENGTH(validation_list)))-1) IS NOT NULL;
 
-INSERT INTO datatype (realm, dttcode, classname, component, inputmask)
-SELECT DISTINCT realm, dttcode, classname, 'text', inputmask FROM `attribute` WHERE dttcode IS NOT NULL AND component IS NULL;
+INSERT INTO datatype (realm, dttcode, classname, component, inputmask, validation_codes)
+SELECT distinct realm, dttcode, classname, 'text', inputmask, SUBSTR(validation_list, 2, POSITION('"' IN SUBSTR(validation_list, 2, CHAR_LENGTH(validation_list)))-1) AS validation_codes 
+FROM `attribute` WHERE dttcode IS NOT NULL AND component IS NULL AND 
+SUBSTR(validation_list, 2, POSITION('"' IN SUBSTR(validation_list, 2, CHAR_LENGTH(validation_list)))-1) IS NOT NULL;
+
