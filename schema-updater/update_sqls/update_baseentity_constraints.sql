4b5a2e5a2e7a5b1035e67dd5b7ae757b5ed1f643
varunshastry <varunshastry@rediffmail.com>
2023-03-17 16:41:28 +1100

    Reordering the schema updates

--- schema-updater/update_sqls/update_baseentity_constraints.sql
+++ schema-updater/update_sqls/update_baseentity_constraints.sql
@@ -2,4 +2,5 @@ ALTER TABLE baseentity ADD CONSTRAINT unique_key UNIQUE KEY(id);
 ALTER TABLE baseentity DROP PRIMARY KEY;
 ALTER TABLE baseentity ADD CONSTRAINT pri_key PRIMARY KEY (realm,code);
 ALTER TABLE baseentity DROP COLUMN dtype;
+DROP TABLE IF EXISTS context;
 ALTER TABLE baseentity DROP COLUMN id;
