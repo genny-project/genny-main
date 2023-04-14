703ce81a2e934400ee1a1fc10473b6ecad998a39
varunshastry <varunshastry@rediffmail.com>
2023-04-14 12:22:26 +1000

    Removing drop column status

--- schema-updater/update_sqls/update_baseentity_attribute_constraints.sql
+++ schema-updater/update_sqls/update_baseentity_attribute_constraints.sql
@@ -8,5 +8,3 @@ ALTER TABLE baseentity_attribute DROP PRIMARY KEY;
 ALTER TABLE baseentity_attribute ADD CONSTRAINT pri_key PRIMARY KEY (attributeCode,baseEntityCode,realm);
 ALTER TABLE baseentity_attribute ADD COLUMN status INT NOT NULL DEFAULT 0;
 UPDATE baseentity_attribute bea JOIN (SELECT realm,code,status FROM baseentity) be ON bea.realm=be.realm AND bea.baseEntityCode=be.code SET bea.status=be.status;
--- Drop status column in baseentity table only after the above update statement is successful
-ALTER TABLE baseentity DROP COLUMN status;
